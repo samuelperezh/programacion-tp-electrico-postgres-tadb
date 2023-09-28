87505aab20439946c871afe1b61a680ea4c08f39
Luisa Flórez <luisa.florezm@upb.edu.co>
2023-09-28 16:00:06 -0500

    Creación de la solución

---
+++ ProgramacionTP_CS_API_PostgreSQL_Dapper/ProgramacionTP_CS_API_PostgreSQL_Dapper/Models/Autobuses.cs
@@ -0,0 +1,31 @@
+﻿namespace ProgramacionTB_CS_API_PostgreSQL_Dapper.Models
+{
+    public class Autobuses
+    {
+        public int Id { get; set; } = 0;
+        public string Autobus { get; set; } = string.Empty;
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            var otroAutobus = (Autobuses)obj;
+
+            return Id == otroAutobus.Id
+                && Autobus.Equals(otroAutobus.Autobus);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 3;
+                hash = hash * 5 + Id.GetHashCode();
+                hash = hash * 5 + (Autobus?.GetHashCode() ?? 0);
+
+                return hash;
+            }
+        }
+    }
+}
---
+++ ProgramacionTP_CS_API_PostgreSQL_Dapper/ProgramacionTP_CS_API_PostgreSQL_Dapper/Models/Horarios.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgramacionTB_CS_API_PostgreSQL_Dapper.Models
+{
+    public class Horarios
+    {
+        public int Id { get; set; } = 0;
+        public string HorarioPico { get; set; } = string.Empty;
+    }
+}
