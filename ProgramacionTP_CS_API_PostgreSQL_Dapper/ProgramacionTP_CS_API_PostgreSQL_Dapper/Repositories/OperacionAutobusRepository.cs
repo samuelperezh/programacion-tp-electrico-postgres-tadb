86d8d68a0d3bc66697d1aace9e10d6631f15a8c7
Luisa Flórez <luisa.florezm@upb.edu.co>
2023-10-05 10:36:02 -0500

    Creación del método GetAssociatedOperationsAsync para el repositorio, interfaz y servicio de OperacionAutobus

--- ProgramacionTP_CS_API_PostgreSQL_Dapper/ProgramacionTP_CS_API_PostgreSQL_Dapper/Repositories/OperacionAutobusRepository.cs
+++ ProgramacionTP_CS_API_PostgreSQL_Dapper/ProgramacionTP_CS_API_PostgreSQL_Dapper/Repositories/OperacionAutobusRepository.cs
@@ -31,6 +31,46 @@ namespace ProgramacionTP_CS_API_PostgreSQL_Dapper.Repositories
                 return resultadoOperacionAutobuses;
             }
         }
+        public async Task<int> GetTotalAssociatedOperationsAsync(int autobus_id, int horario_id)
+        {
+            using (var conexion = contextoDB.CreateConnection())
+            {
+                DynamicParameters parametrosSentencia = new DynamicParameters();
+                parametrosSentencia.Add("@autobus_id", autobus_id,
+                                        DbType.Int32, ParameterDirection.Input);
+                parametrosSentencia.Add("@horario_id", horario_id,
+                                        DbType.Int32, ParameterDirection.Input);
+
+                string sentenciaSQL = "SELECT COUNT(*) " +
+                                      "FROM operacion_autobuses " +
+                                      "WHERE autobus_id = @autobus_id AND horario_id = @horario_id";
+
+                var totalAutobuses = await conexion.QueryFirstAsync<int>(sentenciaSQL,
+                                        parametrosSentencia);
+
+                return totalAutobuses;
+            }
+        }
+
+        public async Task<IEnumerable<OperacionAutobus>> GetAssociatedOperationsAsync(int autobus_id, int horario_id)
+        {
+            using (var conexion = contextoDB.CreateConnection())
+            {
+                DynamicParameters parametrosSentencia = new DynamicParameters();
+                parametrosSentencia.Add("@autobus_id", autobus_id,
+                                        DbType.Int32, ParameterDirection.Input);
+                parametrosSentencia.Add("@horario_id", horario_id,
+                                        DbType.Int32, ParameterDirection.Input);
+
+                string sentenciaSQL = "SELECT autobus_id, horario_id " +
+                                      "FROM operacion_autobuses " +
+                                      "WHERE autobus_id = @autobus_id AND horario_id = @horario_id";
+
+                var resultadoOperaciones = await conexion.QueryAsync<OperacionAutobus>(sentenciaSQL, parametrosSentencia);
+
+                return resultadoOperaciones;
+            }
+        }
 
         public async Task<bool> CreateAsync(OperacionAutobus unaOperacionAutobus)
         {
--- ProgramacionTP_CS_API_PostgreSQL_Dapper/ProgramacionTP_CS_API_PostgreSQL_Dapper/services/OperacionAutobusService.cs
+++ ProgramacionTP_CS_API_PostgreSQL_Dapper/ProgramacionTP_CS_API_PostgreSQL_Dapper/services/OperacionAutobusService.cs
@@ -1,6 +1,7 @@
 using ProgramacionTB_CS_API_PostgreSQL_Dapper.Helpers;
 using ProgramacionTP_CS_API_PostgreSQL_Dapper.Interfaces;
 using ProgramacionTB_CS_API_PostgreSQL_Dapper.Models;
+using ProgramacionTP_CS_API_PostgreSQL_Dapper.Repositories;
 
 namespace ProgramacionTB_CS_API_PostgreSQL_Dapper.Services
 {
@@ -18,104 +19,25 @@ namespace ProgramacionTB_CS_API_PostgreSQL_Dapper.Services
             return await _OperacionAutobusRepository
                 .GetAllAsync();
         }
-        public async Task<OperacionAutobus> CreateAsync(OperacionAutobus unaOperacionAutobus)
+        public async Task<IEnumerable<OperacionAutobus>> GetAssociatedOperationsAsync(int autobus_id, int horario_id)
         {
-            //PREGUNTARLE AL PROFE SI NO ES NECESARIO VALIDAR QUE EL ID DE LA OPERACION AUTOBUS NO EXISTA PREVIAMENTE
-            //Validamos que el autobus_id no exista previamente
-            //var OperacionAutobusExistente = await _OperacionAutobusRepository
-              //  .GetByNameAsync(unaOperacionAutobus.Nombre);
-
-            //if (OperacionAutobusExistente.Id != 0)
-              //  throw new AppValidationException($"Ya existe una operacion de un autobus con el nombre {CargadorExistente.Nombre}");
-            try
+            // Validar que el autobús esté en operación en horario pico
+            if (!IsAutobusInPicoHorario(horario_id))
             {
-                bool resultadoAccion = await _OperacionAutobusRepository
-                    .CreateAsync(unaOperacionAutobus);
-
-                if (!resultadoAccion)
-                    throw new AppValidationException("Operación ejecutada pero no generó cambios en la DB");
-
-                unaOperacionAutobus = await _OperacionAutobusRepository;
-                    //.GetByNameAsync(unCargador.Nombre!);
-            }
-            catch (DbOperationException error)
-            {
-                throw error;
+                throw new AppValidationException($"El autobús no está en operación en horario pico.");
             }
 
-            return OperacionAutobusExistente;
+            // Obtener las operaciones de autobuses asociadas
+            var operaciones = await _OperacionAutobusRepository.GetAssociatedOperationsAsync(autobus_id, horario_id);
+
+            return operaciones;
+        }
+        private bool IsAutobusInPicoHorario(int horario_id)
+        {
+            // Validar si el horario está en horario pico (ID de horario entre 5-9 y 16-20)
+            return (horario_id >= 5 && horario_id <= 9) || (horario_id >= 16 && horario_id <= 20);
         }
 
-        public async Task<OperacionAutobus> UpdateAsync(int _id, Cargador unCargador)
-        {
-            //Validamos que los parametros sean consistentes
-            if (Cargador_id != unCargador.Id)
-                throw new AppValidationException($"Inconsistencia en el Id del cargador a actualizar. Verifica argumentos");
 
-            //Validamos que el cargador exista con ese Id
-            var CargadorExistente = await _CargadorRepository
-                .GetByIdAsync(Cargador_id);
-
-            if (CargadorExistente.Id == 0)
-                throw new AppValidationException($"No existe un Cargador registrado con el id {unCargador.Id}");
-
-            //Validamos que la Autobus tenga nombre
-            if (unCargador.Nombre.Length == 0)
-                throw new AppValidationException("No se puede actualizar un Cargador con nombre nulo");
-
-            //Validamos que el nombre no exista previamente en otro Cargador diferente a la que se está actualizando
-            CargadorExistente = await _CargadorRepository
-                .GetByNameAsync(unCargador.Nombre);
-
-            if (unCargador.Id != CargadorExistente.Id)
-                throw new AppValidationException($"Ya existe otro autobus con el nombre {unCargador.Nombre}. " +
-                    $"No se puede Actualizar");
-
-            //Validamos que haya al menos un cambio en las propiedades
-            if (unCargador.Equals(CargadorExistente))
-                throw new AppValidationException("No hay cambios en los atributos del Cargador. No se realiza actualización.");
-
-            try
-            {
-                bool resultadoAccion = await _CargadorRepository
-                    .UpdateAsync(unCargador);
-
-                if (!resultadoAccion)
-                    throw new AppValidationException("Operación ejecutada pero no generó cambios en la DB");
-
-                CargadorExistente = await _CargadorRepository
-                    .GetByNameAsync(unCargador.Nombre!);
-            }
-            catch (DbOperationException error)
-            {
-                throw error;
-            }
-
-            return CargadorExistente;
-        }
-
-        public async Task DeleteAsync(int Cargador_id)
-        {
-            // validamos que el Cargador a eliminar si exista con ese Id
-            var CargadorExistente = await _CargadorRepository
-                .GetByIdAsync(Cargador_id);
-
-            if (CargadorExistente.Id == 0)
-                throw new AppValidationException($"No existe un Cargador con el Id {Cargador_id} que se pueda eliminar");
-
-            //Si existe se puede eliminar
-            try
-            {
-                bool resultadoAccion = await _CargadorRepository
-                    .DeleteAsync(CargadorExistente);
-
-                if (!resultadoAccion)
-                    throw new AppValidationException("Operación ejecutada pero no generó cambios en la DB");
-            }
-            catch (DbOperationException error)
-            {
-                throw error;
-            }
-        }
     }
 }
